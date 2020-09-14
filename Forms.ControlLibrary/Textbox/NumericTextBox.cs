62f1d6c57d79d4df4d247e2c54050231a84b31ff
Weingart <daniel.weingart@thyssenkrupp.com>
2020-09-14 11:35:29 +0200

    Add project files.

---
+++ Forms.ControlLibrary/Textbox/NumericTextBox.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forms.ControlLibrary.Textbox
+{
+    public partial class NumericTextBox : TextBox
+    {
+        string DecimalSeparator;
+        private bool _AllowOnlyInteger;// = false;
+
+        [Category("Numeric Events")]
+        [Description("Allow decimal seperator")]
+        public bool AllowOnlyInteger
+        {
+            get
+            {
+                return _AllowOnlyInteger;
+            }
+            set
+            {
+                _AllowOnlyInteger = value;
+            }
+        }
+
+        public double Value
+        {
+            get { return Convert.ToDouble(this.Text); }
+        }
+
+        public NumericTextBox()
+        {
+            InitializeComponent();
+            //this.ShortcutsEnabled = false;
+            this.KeyPress += new KeyPressEventHandler(NumericTextBox_KeyPress);
+            this.Leave += new EventHandler(NumericTextBox_Leave);
+            DecimalSeparator = System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+        }
+
+
+        void NumericTextBox_Leave(object sender, EventArgs e)
+        {
+            var value = (TextBox)sender;
+
+            if (value.Text.Equals(DecimalSeparator))
+                value.Text = "0";
+
+            else if (value.Text.IndexOf(DecimalSeparator) == 0 && value.Text.Count() > 1)
+            {
+                value.Text = "0" + value.Text;
+                return;
+            }
+            else if (value.Text.Count() == 0)
+                value.Text = "0";
+
+            else if (value.Text[0] == '0' && value.Text.Count() > 1 && !(value.Text[1] == ','))
+            {
+                var txt = value.Text.Substring(1);
+                var txtCnt = txt.Count();
+                for (int i = 0; i < txtCnt; i++)
+                {
+                    if (txt[0] == '0' && txt.Count() > 1 && !(txt[1] == ','))
+                        txt = txt.Substring(1);
+
+                }
+                value.Text = txt;
+            }
+
+        }
+
+        void NumericTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            var value = (TextBox)sender;
+
+
+            //nur ein , zulassen
+            if (e.KeyChar == Convert.ToChar(DecimalSeparator) && !_AllowOnlyInteger)
+            {
+                if (value.Text.IndexOf(DecimalSeparator) != -1)
+                    e.Handled = true;
+            }
+
+            else if ("-1234567890 \b".IndexOf(e.KeyChar.ToString()) < 0)
+            {
+                //Bei Abweichung Ereignis verwerfen
+                e.Handled = true;
+            }
+
+            if (e.KeyChar == 13 && value.Text.Count() == 0)
+                value.Text = "0";
+
+        }
+    }
+}
---
+++ TestApp/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace TestApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericTextBox1 = new Forms.ControlLibrary.Textbox.NumericTextBox();
+            this.SuspendLayout();
+            // 
+            // numericTextBox1
+            // 
+            this.numericTextBox1.AllowOnlyInteger = false;
+            this.numericTextBox1.Location = new System.Drawing.Point(207, 146);
+            this.numericTextBox1.Name = "numericTextBox1";
+            this.numericTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.numericTextBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Forms.ControlLibrary.Textbox.NumericTextBox numericTextBox1;
+    }
+}
+
